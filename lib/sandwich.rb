4788ec041e8212b59f6d2262adb29c061427b86d
DefyCab <love.bellinder@gmail.com>
2021-11-14 15:45:57 +0100

    add let to sandwich. refactoring

--- lib/sandwich.rb
+++ lib/sandwich.rb
@@ -1,7 +1,9 @@
 class Sandwich
-attr_accessor :meat, :cheese, :toppings
+  attr_accessor :meat, :cheese, :toppings
 
-    def initialize(meat, cheese, toppings=[])
+  def initialize(meat, cheese, toppings = [])
+    self.meat = meat
+    self.cheese = cheese
+    self.toppings = toppings
+  end
 end
-end
-
--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,32 +1,33 @@
 require "./lib/sandwich.rb"
 
 describe Sandwich do
+  context "when the sandwich should be vegan" do
+    let(:sandwich) { Sandwich.new(false, false, ["lettuce", "tomato", "mustard"] )}
 
-    context "when the sandwich should be vegan" do
+    #the symbol sandwich refarence this variable.
+    #Whatever evaluated saved in this variable.
+    #It applies to all it-blocks.
 
-        it "shold not have meat" do
-            sandwich = Sandwich.new(false, false, ['lettuce','tomato', 'mustard'])
-            expect(sandwich.meat).to eq(false)
-            end
-
-        it "should not have cheese" do
-            sandwich = Sandwich.new(false, false, ['lettuce','tomato', 'mustard'])
-            expect(sandwich.cheese).to eq(false)
-        end
-
-
-        it "should not have mustard" do
-            sandwich = Sandwich.new(false, false, ['lettuce','tomato', 'mustard'])
-             expect(sandwich.toppings).to_not include("mustard")
-
-        end
-    end    
-    context "when the sandwich should not be vegan" do
-
-        it "shold have meat" 
-    
-        it "should have cheese"
-
-        it "should have mayonaise"
+    it "shold not have meat" do
+      #sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
+      expect(sandwich.meat).to eq(false)
     end
+
+    it "should not have cheese" do
+      #sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
+      expect(sandwich.cheese).to eq(false)
+    end
+
+    it "should not have mustard" do
+      #sandwich = Sandwich.new(false, false, ["lettuce", "tomato", "mustard"])
+      expect(sandwich.toppings).to_not include("mayonaise")
+    end
+  end
+  context "when the sandwich should not be vegan" do
+    it "shold have meat"
+
+    it "should have cheese"
+
+    it "should have mayonaise"
+  end
 end
